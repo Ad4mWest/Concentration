3a65a074eed0e14aa4d600f3b41591b77c92b8a6
Ad4mWest <adam101west@icloud.com>
2023-04-28 14:45:47 +0200

    Created a project in the Xcode program - Created the visual part of the UI program - Connected the visual parts of the program to the code - Gave functionality to visual parts by writing code

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -3,17 +3,39 @@
 //  Concentration
 //
 //  Created by adam west on 27.04.23.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var touches = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
     }
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.label
+            } else {
+                button.titleLabel?.font = UIFont.systemFont(ofSize: 50)
+                button.setTitle(emoji, for: .normal)
+                button.backgroundColor = UIColor.white
+            }
+        }
 
-
+    let emojiCollection = ["🦊", "🐰", "🦊", "🐰"]
+    
+    
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLabel: UILabel!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: "\(emojiCollection[buttonIndex])", button: sender)
+        }
+    }
+    
 }
 
